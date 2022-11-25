07b82c67a264a9cee7bc328c7c9aa52063a9e0fa
Bigkid6999 <J.fields@share.epsb.ca>
2022-11-24 22:50:01 -0700

    Update main.js

--- main.js
+++ main.js
@@ -4,5 +4,25 @@
 document.getElementById("btn").addEventListener("click", btnClicked);
 
 function btnClicked() {
-  console.log("Search");
+// Input
+let name = document.getElementById("char-in").value;
+  console.log("name");
+if(name === "Po") {
+document.getElementById(char-name).innerHTML = "Po";
+document.getElementById(char-quote).innerHTML = "Buddy I am the dragon warrior ";
+document.getElementById(char-img).src = "img/po.png"
+} else if (name===Tigress) {
+  document.getElementById(char-name).innerHTML = "Tigress";
+document.getElementById(char-quote).innerHTML = "That was hard core ";
+document.getElementById(char-img).src = "img/tigress.png"
+} else {
+console.log("?")
+} else if (name===Mantis) {
+  console.log("Yes Mantis  is a kung fu panda character");
+
+
+  
+
+
 }
+
