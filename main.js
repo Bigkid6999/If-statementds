de8f6c6a3b0d4686f6cfb0e5ff79fc5c620b1765
Bigkid6999 <112982143+Bigkid6999@users.noreply.github.com>
2022-12-13 13:37:51 -0700

    Update main.js

--- main.js
+++ main.js
@@ -4,25 +4,21 @@
 document.getElementById("btn").addEventListener("click", btnClicked);
 
 function btnClicked() {
-// Input
-let name = document.getElementById("char-in").value;
+  // Input
+  let name = document.getElementById("char-in").value;
   console.log("name");
-if(name === "Po") {
-document.getElementById(char-name).innerHTML = "Po";
-document.getElementById(char-quote).innerHTML = "Buddy I am the dragon warrior ";
-document.getElementById(char-img).src = "img/po.png"
-} else if (name===Tigress) {
-  document.getElementById(char-name).innerHTML = "Tigress";
-document.getElementById(char-quote).innerHTML = "That was hard core ";
-document.getElementById(char-img).src = "img/tigress.png"
-} else {
-console.log("?")
-} else if (name===Mantis) {
-  console.log("Yes Mantis  is a kung fu panda character");
+  if (name === "Po") {
+    document.getElementById(char - name).innerHTML = "Po";
+    document.getElementById(char - quote).innerHTML = "Buddy I am the dragon warrior ";
+    document.getElementById(char - img).src = "img/po.png"
+  } else if (name === Tigress) {
+    document.getElementById(char - name).innerHTML = "Tigress";
+    document.getElementById(char - quote).innerHTML = "That was hard core ";
+    document.getElementById(char - img).src = "img/tigress.png"
+  } else if (name === Mantis) {
+    console.log("Yes Mantis  is a kung fu panda character")
 
-
-  
-
-
+  }else {
+    console.log("?")
+  } 
 }
-
